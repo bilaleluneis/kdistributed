64a1fac5585480da6172d18888583425e40d30b2
bilaleluneis <bilaleluneis@gmail.com>
2022-10-10 08:05:03 -0700

    - removed a completed TODO

--- src/main/kotlin/common/Utils.kt
+++ src/main/kotlin/common/Utils.kt
@@ -11,7 +11,6 @@ enum class IDType(val length: Int) {
     UUID(6),
 }
 
-// TODO: introduce const or enum to hold length for uuid , grpid,etc
 fun genId(type: IDType): String {
     val combChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
     return (1 until type.length).map { combChars.random() }.joinToString("")
