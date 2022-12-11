18f83a62f8793f32b70c7bcbfa6029a0bf14971c
bilaleluneis <bilaleluneis@gmail.com>
2022-12-10 18:33:02 -0700

    - resolve out of bound exception caused by toDataOnly() when called on node with empty list. - added arrow-kt api to take advantage of Either<> and catch{} for exception handling in functional code. - modified list<Host>.consume() to use arrow-kt catch to handle thrown exception, if a node is down instead of throwing and killing entire process when there might be nodes available to honor requests.

--- src/main/kotlin/org/distributed/common/Types.kt
+++ src/main/kotlin/org/distributed/common/Types.kt
@@ -23,13 +23,20 @@ sealed class Data : Serializable
 data class DataOnly<T>(val data: T) : Data()
 data class DataWithUuiD<T>(val data: T, val uuid: UuID) : Data()
 
-fun <T> List<Data>.toDataOnly(): List<DataOnly<T>> = when (this[0]) {
-    is DataOnly<*> -> this.filterIsInstance<DataOnly<T>>()
-    is DataWithUuiD<*> -> this.filterIsInstance<DataWithUuiD<T>>().map { DataOnly(it.data) }
+fun <T> List<Data>.toDataOnly(): List<DataOnly<T>> {
+    if (isEmpty()) return emptyList()
+    return when (this[0]) {
+        is DataOnly<*> -> this.filterIsInstance<DataOnly<T>>()
+        is DataWithUuiD<*> -> this.filterIsInstance<DataWithUuiD<T>>().map { DataOnly(it.data) }
+    }
 }
 
-fun <T> List<T>.toData(): List<Data> = this.map { DataOnly(it) }
+fun <T> List<T>.toData(): List<Data> = map { DataOnly(it) }
 
 // all distributed types interfaces will need to implement this
 interface Distributed : Remote
+
+
+
+
 
--- src/main/kotlin/org/distributed/types/Utils.kt
+++ src/main/kotlin/org/distributed/types/Utils.kt
@@ -5,28 +5,39 @@
  */
 package org.distributed.types
 
-import org.distributed.common.Distributed
-import org.distributed.common.Host
+import arrow.core.Either
+import arrow.core.Either.Companion.catch
+import org.distributed.common.*
 import java.net.InetAddress
 import java.rmi.RemoteException
 import java.rmi.registry.LocateRegistry.createRegistry
 import java.rmi.registry.LocateRegistry.getRegistry
 import java.rmi.server.UnicastRemoteObject.exportObject
 
-
 @Throws(RemoteException::class)
 inline fun <reified T : Distributed> List<Host>.consume(): List<T> {
-    return map{ getRegistry(it.ip, it.port).lookup(T::class.simpleName) as T }.toList()
+    return mapNotNull {
+        when (val c = catch { getRegistry(it.ip, it.port).lookup(T::class.simpleName) }) {
+            is Either.Left -> {
+                println("Host: ${it.name} not availabe due ${c.value.message}")
+                null
+            }
+            is Either.Right -> c.value as T
+        }
+    }
 }
 
 //TODO: is there a way to publish same type to multiple ports without getting Exported Exception ?
 @Throws(RemoteException::class)
-inline fun <reified T: Distributed> List<Host>.publish(dtype: T) {
+inline fun <reified T : Distributed> List<Host>.publish(dtype: T) {
     first { it.name == InetAddress.getLocalHost().hostName }.apply {
         System.setProperty("java.rmi.server.hostname", ip)
         createRegistry(port).bind(T::class.simpleName, exportObject(dtype, port))
     }
 }
+
+
+
 
 
 
